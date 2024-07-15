ed612fe4bc2858d0a1137e9de966545c96f17ce1
纪宇航 <jiyuhang@jctrans.net>
2024-07-15 16:49:51 +0800

    feat: init index page

---
+++ app/components/HeroSection.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container mx-auto pb-12 px-4">
+    <div
+      class="bg-center bg-cover bg-no-repeat blur-none rounded-3xl z-0"
+      :style="bgStyles"
+    >
+      <div class>
+        <div class="lg:px-12 md:py-36 pb-6 pt-72 px-6 relative rounded-3xl">
+          <div
+            class="-mx-4 flex flex-wrap items-center mt-24 space-y-6 lg:space-y-0"
+          >
+            <div class="px-4 w-full md:w-8/12 xl:w-6/12">
+              <h4
+                class="font-bold font-serif mb-1 text-primary-600 dark:text-primary-200 uppercase"
+              >
+                Adopt A Pet
+              </h4>
+              <h1 class="capitalize leading-tight mb-2 text-white">
+                Thousands of homeless dogs, one act of kindness
+              </h1>
+              <p class="font-thin mb-6 text-gray-300">
+                Find your new furry friend today! We have a wide variety of
+                lovable pets available for adoption. Come visit us and take home
+                your new best friend!
+              </p>
+              <BaseButton
+                size="xl"
+                label="Adopt Now"
+                target="_blank"
+                to="https://github.com/pinegrow/happy-paws-with-nuxt-tailwindcss"
+              >
+                <div class="inline-block ml-1 p-1.5 text-lg">Adopt Now</div>
+                <BaseIcon name="i-emojione-v1-dog-face" height="28px" />
+              </BaseButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { heroImageUrl } from '@/utils/hero'
+
+  const { optimizeImage } = useOptimizeImage()
+  console.log('🚀 ~ file: HeroSection.vue:50 ~ optimizeImage:', optimizeImage)
+  const herpImageOptimized = {
+    alt: `hero`,
+    cover: true,
+    ...optimizeImage(
+      heroImageUrl,
+      {
+        placeholder: false,
+      },
+      true,
+    ),
+  }
+  const heroImage = herpImageOptimized.src
+  const bgStyles = herpImageOptimized.bgStyles
+</script>
+
+<style></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>index</div>
+  <div>
+    <HeroSection />
+  </div>
 </template>
 
 <script lang="ts" setup></script>
