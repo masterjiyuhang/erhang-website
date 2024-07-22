fc3129132132a8b37bba6b629b0edfefac8ea976
纪宇航 <jiyuhang@jctrans.net>
2024-07-22 11:36:55 +0800

    feat: update layout footer

--- nuxt-tailwindcss-layer/components/TheFooter.vue
+++ nuxt-tailwindcss-layer/components/TheFooter.vue
@@ -1,5 +1,95 @@
 <script setup lang="ts">
   // ...
+
+  const iconList = ref([
+    {
+      href: 'https://www.facebook.com/vuedesigner',
+      name: 'facebook',
+      icon: 'i-logos-facebook',
+    },
+    {
+      href: 'https://twitter.com/vuedesigner',
+      name: 'twitter',
+      icon: 'i-logos-twitter',
+    },
+    {
+      href: 'https://discord.gg/BYp45Nnu5T',
+      name: 'discord',
+      icon: 'i-logos-discord-icon',
+    },
+    {
+      href: 'https://www.youtube.com/@vuedesigner',
+      name: 'youtube',
+      icon: 'i-logos-youtube-icon',
+    },
+  ])
+
+  const generalList = ref([
+    {
+      label: 'FAQ',
+      path: '/',
+    },
+    {
+      label: 'News',
+      path: '/',
+    },
+    {
+      label: 'Careers',
+      path: '/',
+    },
+    {
+      label: 'Contact Us',
+      path: '/',
+    },
+  ])
+
+  const linksList = ref([
+    {
+      label: 'Adopt Now',
+      path: '/adopt',
+    },
+    {
+      label: 'QuickStart',
+      path: '/quick-start',
+    },
+    {
+      label: 'Articles',
+      path: '/articles',
+    },
+    {
+      label: 'Blog',
+      path: '/blog',
+    },
+    {
+      label: 'Volunteer',
+      path: '/volunteer',
+    },
+    {
+      label: 'Our Mission',
+      path: '#',
+    },
+  ])
+
+  const imgList = ref([
+    {
+      path: 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDF8fHBldHxlbnwwfHx8fDE2MzA0NjI1MjM&ixlib=rb-1.2.1q=85&fm=jpg&crop=faces&cs=srgb&w=300&h=250&fit=crop',
+    },
+    {
+      path: 'https://images.unsplash.com/photo-1491485880348-85d48a9e5312?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDE1fHxjYXR8ZW58MHx8fHwxNjMwNDY3NzE4&ixlib=rb-1.2.1q=85&fm=jpg&crop=faces&cs=srgb&w=300&h=250&fit=crop',
+    },
+    {
+      path: 'https://images.unsplash.com/photo-1591946614720-90a587da4a36?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDIzfHxwZXR8ZW58MHx8fHwxNjMwNDYyNTIz&ixlib=rb-1.2.1q=85&fm=jpg&crop=faces&cs=srgb&w=300&h=250&fit=crop',
+    },
+    {
+      path: 'https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDI3fHxjYXR8ZW58MHx8fHwxNjMwNDY3NzE4&ixlib=rb-1.2.1q=85&fm=jpg&crop=faces&cs=srgb&w=300&h=250&fit=crop',
+    },
+    {
+      path: 'https://images.unsplash.com/photo-1524511751214-b0a384dd9afe?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDI4fHxwZXR8ZW58MHx8fHwxNjMwNDYyNTIz&ixlib=rb-1.2.1q=85&fm=jpg&crop=faces&cs=srgb&w=300&h=250&fit=crop',
+    },
+    {
+      path: 'https://images.unsplash.com/photo-1591561582301-7ce6588cc286?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDMwfHxyYWJiaXR8ZW58MHx8fHwxNjMwNDY3NjUw&ixlib=rb-1.2.1q=85&fm=jpg&crop=faces&cs=srgb&w=300&h=250&fit=crop',
+    },
+  ])
 </script>
 
 <template>
@@ -28,52 +118,14 @@
           <div class="inline-flex mt-6 space-x-3">
             <div class="flex flex-wrap space-x-4">
               <NuxtLink
-                href="https://www.facebook.com/vuedesigner"
-                aria-label="facebook"
+                v-for="item in iconList"
+                :key="item.name"
+                :href="item.href"
+                :aria-label="item.name"
                 external
                 target="_blank"
               >
-                <BaseIcon
-                  name="i-logos-facebook"
-                  class="text-2xl"
-                  width="28px"
-                />
-              </NuxtLink>
-              <NuxtLink
-                href="https://twitter.com/vuedesigner"
-                aria-label="twitter"
-                external
-                target="_blank"
-              >
-                <BaseIcon
-                  name="i-logos-twitter"
-                  class="text-2xl"
-                  width="28px"
-                />
-              </NuxtLink>
-              <NuxtLink
-                href="https://discord.gg/BYp45Nnu5T"
-                aria-label="discord"
-                external
-                target="_blank"
-              >
-                <BaseIcon
-                  name="i-logos-discord-icon"
-                  class="text-2xl"
-                  width="28px"
-                />
-              </NuxtLink>
-              <NuxtLink
-                href="https://www.youtube.com/@vuedesigner"
-                aria-label="youtube"
-                external
-                target="_blank"
-              >
-                <BaseIcon
-                  name="i-logos-youtube-icon"
-                  class="text-2xl"
-                  width="28px"
-                />
+                <BaseIcon :name="item.icon" class="text-2xl" width="28px" />
               </NuxtLink>
             </div>
           </div>
@@ -89,32 +141,11 @@
             General
           </h2>
           <ul class="text-center lg:mt-2 lg:text-left">
-            <li class="mb-4">
+            <li v-for="item in generalList" :key="item.label" class="mb-4">
               <NuxtLink
-                to="/"
+                :to="item.path"
                 class="hover:text-primary-600 dark:text-primary-200"
-                >FAQ</NuxtLink
-              >
-            </li>
-            <li class="mb-4">
-              <NuxtLink
-                to="/"
-                class="hover:text-primary-600 dark:text-primary-200"
-                >News</NuxtLink
-              >
-            </li>
-            <li class="mb-4">
-              <NuxtLink
-                to="/"
-                class="hover:text-primary-600 dark:text-primary-200"
-                >Careers</NuxtLink
-              >
-            </li>
-            <li class="mb-4">
-              <NuxtLink
-                to="/"
-                class="hover:text-primary-600 dark:text-primary-200"
-                >Contact Us</NuxtLink
+                >{{ item.label }}</NuxtLink
               >
             </li>
           </ul>
@@ -126,46 +157,11 @@
             Links
           </h2>
           <ul class="text-center lg:mt-2 lg:text-left">
-            <li class="mb-4">
+            <li v-for="item in linksList" :key="item.label" class="mb-4">
               <NuxtLink
-                to="/adopt"
+                :to="item.path"
                 class="hover:text-primary-600 dark:text-primary-200"
-                >Adopt Now</NuxtLink
-              >
-            </li>
-            <li class="mb-4">
-              <NuxtLink
-                to="/quick-start"
-                class="hover:text-primary-600 dark:text-primary-200"
-                >QuickStart</NuxtLink
-              >
-            </li>
-            <li class="mb-4">
-              <NuxtLink
-                to="/articles"
-                class="hover:text-primary-600 dark:text-primary-200"
-                >Articles</NuxtLink
-              >
-            </li>
-            <li class="mb-4">
-              <NuxtLink
-                to="/blog"
-                class="hover:text-primary-600 dark:text-primary-200"
-                >Blog</NuxtLink
-              >
-            </li>
-            <li class="mb-4">
-              <NuxtLink
-                to="/volunteer"
-                class="hover:text-primary-600 dark:text-primary-200"
-                >Volunteer</NuxtLink
-              >
-            </li>
-            <li class="mb-4">
-              <NuxtLink
-                to="#"
-                class="hover:text-primary-600 dark:text-primary-200"
-                >Our Mission</NuxtLink
+                >{{ item.label }}</NuxtLink
               >
             </li>
           </ul>
@@ -175,52 +171,13 @@
             Gallery
           </h2>
           <div class="-mx-2 flex flex-wrap items-center lg:mt-2">
-            <div class="p-2 w-1/2 sm:w-4/12">
+            <div
+              v-for="(item, index) in imgList"
+              :key="index"
+              class="p-2 w-1/2 sm:w-4/12"
+            >
               <div class="block hover:opacity-75 overflow-hidden rounded-xl">
-                <NuxtImg
-                  src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDF8fHBldHxlbnwwfHx8fDE2MzA0NjI1MjM&amp;ixlib=rb-1.2.1q=85&amp;fm=jpg&amp;crop=faces&amp;cs=srgb&amp;w=300&amp;h=250&amp;fit=crop"
-                  class="w-full"
-                />
-              </div>
-            </div>
-            <div class="p-2 w-1/2 sm:w-4/12">
-              <div class="block hover:opacity-75 overflow-hidden rounded-xl">
-                <NuxtImg
-                  src="https://images.unsplash.com/photo-1491485880348-85d48a9e5312?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDE1fHxjYXR8ZW58MHx8fHwxNjMwNDY3NzE4&amp;ixlib=rb-1.2.1q=85&amp;fm=jpg&amp;crop=faces&amp;cs=srgb&amp;w=300&amp;h=250&amp;fit=crop"
-                  class="w-full"
-                />
-              </div>
-            </div>
-            <div class="p-2 w-1/2 sm:w-4/12">
-              <div class="block hover:opacity-75 overflow-hidden rounded-xl">
-                <NuxtImg
-                  src="https://images.unsplash.com/photo-1591946614720-90a587da4a36?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDIzfHxwZXR8ZW58MHx8fHwxNjMwNDYyNTIz&amp;ixlib=rb-1.2.1q=85&amp;fm=jpg&amp;crop=faces&amp;cs=srgb&amp;w=300&amp;h=250&amp;fit=crop"
-                  class="w-full"
-                />
-              </div>
-            </div>
-            <div class="p-2 w-1/2 sm:w-4/12">
-              <div class="block hover:opacity-75 overflow-hidden rounded-xl">
-                <NuxtImg
-                  src="https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDI3fHxjYXR8ZW58MHx8fHwxNjMwNDY3NzE4&amp;ixlib=rb-1.2.1q=85&amp;fm=jpg&amp;crop=faces&amp;cs=srgb&amp;w=300&amp;h=250&amp;fit=crop"
-                  class="w-full"
-                />
-              </div>
-            </div>
-            <div class="p-2 w-1/2 sm:w-4/12">
-              <div class="block hover:opacity-75 overflow-hidden rounded-xl">
-                <NuxtImg
-                  src="https://images.unsplash.com/photo-1524511751214-b0a384dd9afe?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDI4fHxwZXR8ZW58MHx8fHwxNjMwNDYyNTIz&amp;ixlib=rb-1.2.1q=85&amp;fm=jpg&amp;crop=faces&amp;cs=srgb&amp;w=300&amp;h=250&amp;fit=crop"
-                  class="w-full"
-                />
-              </div>
-            </div>
-            <div class="p-2 w-1/2 sm:w-4/12">
-              <div class="block hover:opacity-75 overflow-hidden rounded-xl">
-                <NuxtImg
-                  src="https://images.unsplash.com/photo-1591561582301-7ce6588cc286?ixid=MnwyMDkyMnwwfDF8c2VhcmNofDMwfHxyYWJiaXR8ZW58MHx8fHwxNjMwNDY3NjUw&amp;ixlib=rb-1.2.1q=85&amp;fm=jpg&amp;crop=faces&amp;cs=srgb&amp;w=300&amp;h=250&amp;fit=crop"
-                  class="w-full"
-                />
+                <NuxtImg :src="item.path" class="w-full" />
               </div>
             </div>
           </div>
@@ -232,40 +189,6 @@
           </p>
         </div>
       </div>
-    </div>
-    <div class="flex flex-wrap justify-center space-x-8">
-      <NuxtLink
-        href="https://www.facebook.com/vuedesigner"
-        aria-label="facebook"
-        external
-        target="_blank"
-      >
-        <BaseIcon name="i-logos-facebook" class="text-2xl" width="28px" />
-      </NuxtLink>
-      <NuxtLink
-        href="https://twitter.com/vuedesigner"
-        aria-label="twitter"
-        external
-        target="_blank"
-      >
-        <BaseIcon name="i-logos-twitter" class="text-2xl" width="28px" />
-      </NuxtLink>
-      <NuxtLink
-        href="https://discord.gg/BYp45Nnu5T"
-        aria-label="discord"
-        external
-        target="_blank"
-      >
-        <BaseIcon name="i-logos-discord-icon" class="text-2xl" width="28px" />
-      </NuxtLink>
-      <NuxtLink
-        href="https://www.youtube.com/@vuedesigner"
-        aria-label="youtube"
-        external
-        target="_blank"
-      >
-        <BaseIcon name="i-logos-youtube-icon" class="text-2xl" width="28px" />
-      </NuxtLink>
     </div>
   </footer>
 </template>
