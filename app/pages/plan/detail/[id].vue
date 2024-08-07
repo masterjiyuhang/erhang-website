8eb410e4b0a375873a1d878e1742a7fdcfd06568
纪宇航 <jiyuhang@jctrans.net>
2024-08-07 09:21:11 +0800

    feat: update plan detail

--- app/pages/plan/detail/[id].vue
+++ app/pages/plan/detail/[id].vue
@@ -1,8 +1,14 @@
+<!-- eslint-disable vue/no-v-html -->
 <template>
-  <div>
-    <h1>Plan Detail</h1>
-    <p>Plan ID: {{ id }}</p>
-    <div>currentItem: {{ currentItem }}</div>
+  <div class="w-[1200px] mx-auto flex flex-col my-3">
+    <!-- <h1>Plan Detail</h1> -->
+    <!-- <p>Plan ID: {{ id }}</p> -->
+    <div>
+      <!-- currentItem: {{ currentItem }} -->
+      <h2>{{ getCnOrEn(currentItem.titleCn, currentItem.titleEn) }}</h2>
+      <h3>{{ currentItem.createTime }}</h3>
+      <div v-html="getCnOrEn(currentItem.contextCn, currentItem.contextEn)" />
+    </div>
   </div>
 </template>
 
@@ -12,10 +18,23 @@
 
   const { getPlanList } = usePlanList()
   const { data } = await getPlanList()
-  console.log('🚀 ~ file: [id].vue:12 ~ data:', data.value)
 
   const currentItem = computed(() => {
     return data.value.data.records.find((item) => item.id === Number(id))
+  })
+
+  definePageMeta({
+    title: 'Plan Detail',
+    description: 'Read, Learn, Enjoy: Your Blog Destination!',
+  })
+
+  useHead({
+    meta: [
+      {
+        name: 'description',
+        content: 'plan detail',
+      },
+    ],
   })
 </script>
 
