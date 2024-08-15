80cfa9205fe893bc8f4d40779f755819379c442d
纪宇航 <jiyuhang@jctrans.net>
2024-08-15 11:38:46 +0800

    feat: update nav menu page

--- app/pages/nav-menu/index.vue
+++ app/pages/nav-menu/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div />
+  <div class="mx-auto bg-secondary-500 lg:min-h-full max-w-[1200px]">
+    Nav Menu
+  </div>
 </template>
 
 <script lang="ts" setup>
   definePageMeta({
     title: 'Nav Menu',
-    description: '3',
+    description: 'Global website nav menu.',
     type: 'secondary',
     navOrder: 7,
+  })
+
+  useSeoMeta({
+    ogDescription: 'Global website nav menu.',
+    description: 'Nav Menu.',
   })
 </script>
 
