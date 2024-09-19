db34bcd29571ac2115b4e575403d3069b7a8c273
纪宇航 <jiyuhang@jctrans.net>
2024-09-19 19:10:52 +0800

    fix: 19:10

--- nuxt-tailwindcss-layer/components/menu/TopMenuBar.vue
+++ nuxt-tailwindcss-layer/components/menu/TopMenuBar.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="container">
-    <!-- <div @click="navigateToLocalizedPath('/')">
+    <div @click="navigateToLocalizedPath('/')">
       <NuxtImg
         src="/images/panada2.png"
         class="w-[154px] h-[41px] cursor-pointer"
       />
-    </div> -->
-
-    <div>
-      <PopoverJoinUs />
     </div>
+
+    <!-- <div>
+      <PopoverJoinUs />
+    </div> -->
   </div>
 </template>
 
 <script lang="ts" setup>
-  import PopoverJoinUs from '../../PopoverJoinUs.vue'
-  // const { navigateToLocalizedPath } = usePath()
+  // import PopoverJoinUs from '../../PopoverJoinUs.vue'
+  const { navigateToLocalizedPath } = usePath()
 </script>
 
 <style></style>
