e354384351951c89dcd74d03f895b1a49320163f
纪宇航 <jiyuhang@jctrans.net>
2024-10-13 21:53:42 +0800

    feat: update home pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,28 +1,19 @@
 <template>
   <div class="-top-24 relative">
     <HomeSearch />
-    <ClientOnly>
-      <HomeTopBanner />
-      <HomeRecommendMerchant />
-      <HomeSeaTransport />
-      <HomeAirTransport />
-      <HomeLandTransport />
-      <HomeComprehensiveServices />
-      <HomeOverseasAgents />
-      <div class="container p-0 space-x-5 flex h-[45px] mt-4">
-        <div class="flex-1 h-full bg-primary-100" />
-        <div class="w-[230px] h-full bg-primary-100" />
-      </div>
+    <HomeTopBanner />
+    <HomeRecommendMerchant />
+    <HomeSeaTransport />
+    <HomeAirTransport />
+    <HomeLandTransport />
+    <HomeComprehensiveServices />
+    <HomeOverseasAgents />
+    <div class="container p-0 space-x-5 flex h-[45px] mt-4">
+      <div class="flex-1 h-full bg-primary-100" />
+      <div class="w-[230px] h-full bg-primary-100" />
+    </div>
 
-      <HomeTools class="mt-4" />
-    </ClientOnly>
-
-    <div
-      class="h-[235px] w-[90px] bg-[#7e7e7e] absolute top-[370px] left-0 xl:left-[8%] hidden 2xl:block"
-    />
-    <div
-      class="h-[235px] w-[90px] bg-[#7e7e7e] absolute top-[370px] right-0 xl:right-[8%] hidden 2xl:block"
-    />
+    <HomeTools class="mt-4" />
   </div>
 </template>
 
