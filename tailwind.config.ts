ffaaa1bcfaa4795bfde5a8a130477c086c42eabe
纪宇航 <jiyuhang@jctrans.net>
2025-06-16 20:09:17 +0800

    chore: up deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,8 @@
 import tailwindTypography from '@tailwindcss/typography'
 import tailwindForms from '@tailwindcss/forms'
-import tailwindCssPluginPinegrow from '@pinegrow/tailwindcss-plugin'
 import tailwindAspectRatio from '@tailwindcss/aspect-ratio'
 import tailwindHeadless from '@headlessui/tailwindcss'
 
-import {
-  pg_colors,
-  pg_fonts,
-  pg_backgrounds,
-} from './themes/pg-tailwindcss/tokens.mjs'
-
-import { getFontsWithFallback } from './app/utils/font'
-import { safelist } from './app/utils/colors'
-
-/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
   theme: {
@@ -41,26 +30,6 @@ export default {
       strategy: 'class',
     }),
     tailwindAspectRatio,
-    tailwindCssPluginPinegrow({
-      colors: pg_colors, // primary, secondary etc
-      fonts: getFontsWithFallback(pg_fonts),
-      backgrounds: pg_backgrounds, // bg-design-image, bg-design-image-large
-    }),
     tailwindHeadless,
   ],
-  safelist,
-  get content() {
-    const _content = [
-      '{.,app,*-layer}/components/**/*.{js,vue,ts}',
-      '{.,app,*-layer}/layouts/**/*.vue',
-      '{.,app,*-layer}/pages/**/*.vue',
-      '{.,app,*-layer}/plugins/**/*.{js,ts}',
-      '{.,app,*-layer}/app.vue',
-      '{.,app,*-layer}/*.{mjs,js,ts}',
-      '{.,*-layer}/nuxt.config.{js,ts}',
-    ]
-    return process.env.NODE_ENV === 'production'
-      ? _content
-      : [..._content, './_pginfo/**/*.{html,js}']
-  },
 }
